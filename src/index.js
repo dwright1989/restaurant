083161a0144fac2f65e1afeac14fe81d075d924a
Debbie Wright <debbiewright@live.co.uk>
2022-09-12 10:11:42 +0100

    Adding banner through webpack modules

--- src/index.js
+++ src/index.js
@@ -2,3 +2,37 @@ import './style.css';
 import bannerImg from './background.jpg';
 import drinkImg from './drink.jpg';
 import burgerImg from './burger.jpg';
+
+
+/*
+This file loads the page and navigation bar*/
+
+import loadFullPage from './modules/initial';
+/*import loadHomePage from './modules/home';
+import loadMenuPage from './modules/menu';
+import loadContactPage from './modules/contact';*/
+
+/* Add event listeners to the navigation bar */
+
+loadPage();
+
+/* Load the page*/
+
+function loadPage(){
+    loadFullPage();
+    //loadHomePage();
+    //addEL();
+}
+
+function addEL(){
+    const homeButton = document.getElementById('home');
+    const menuButton = document.getElementById('menu');
+    const contactButton = document.getElementById('contact');
+
+    homeButton.addEventListener('click', loadHomePage);
+    menuButton.addEventListener('click', loadMenuPage);
+    contactButton.addEventListener('click', loadContactPage );
+}
+
+
+
---
+++ src/modules/initial.js
@@ -0,0 +1,57 @@
+
+/* Load the page (common aspects and initial load)*/
+function loadFullPage(){
+    let content = document.getElementById("content");
+    let commonContentDiv = loadHeader();
+    content.appendChild(commonContentDiv);
+}
+
+function loadHeader(){
+    let commonContentDiv = document.createElement("div");
+    commonContentDiv.id = "common-content";
+
+    let bannerDiv = document.createElement("div");
+    bannerDiv.id = "banner";
+    let header = document.createElement("h1");
+    header.innerHTML = "Billy's Bar and Burgers";
+
+    bannerDiv.appendChild(header);
+    commonContentDiv.appendChild(bannerDiv);
+
+    // Navigation buttons
+    let navigation = loadNavigation();
+    commonContentDiv.appendChild(navigation);
+
+    return commonContentDiv;
+}
+
+function loadNavigation(){
+    let navigationDiv = document.createElement("div");
+    navigationDiv.id = "navigation-buttons";
+
+    // Create the links
+    let homeLink = document.createElement("a");
+    homeLink.classList.add("tab");
+    homeLink.classList.add("active-tab");
+    homeLink.id = "homeLink";
+    homeLink.innerHTML= "Home";
+
+    let menuLink = document.createElement("a");
+    menuLink.classList.add("tab");
+    menuLink.id = "menuLink";
+    menuLink.innerHTML= "Menu";
+
+    let contactLink = document.createElement("a");
+    contactLink.classList.add("tab");
+    contactLink.id = "contactLink";
+    contactLink.innerHTML= "Contact";
+
+    navigationDiv.appendChild(homeLink);
+    navigationDiv.appendChild(menuLink);
+    navigationDiv.appendChild(contactLink);
+
+    return navigationDiv;
+
+}
+
+export default loadFullPage;
